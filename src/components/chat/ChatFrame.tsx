e9befb0747101d3d8e5fad0321bef1071b00fb0b
yangbo <1051435548@qq.com>
2020-03-21 12:51:21 +0800

    样式调整

--- src/components/chat/ChatFrame.tsx
+++ src/components/chat/ChatFrame.tsx
@@ -86,61 +86,21 @@ class ChatFrame extends Component<Props> {
                 />
 
                 {/*头像列*/}
-                <div style={{
-                    textAlign: "center",
-                    width: 825,
-                    height: 60,
-                    margin: '0 auto',
-                    marginTop: 20,
-                    display: "flex",
-                    flexDirection: "row",
-                    boxShadow: '-2px -3px 7px -6px rgba(0,0,0,0.8)',
-                    borderTopLeftRadius: 5
-                }}>
-                    <div style={{
-                        width: "25%",
-                        display: "flex",
-                        justifyContent: "space-around",
-                        alignItems: "center",
-                        borderTopLeftRadius: 5,
-                        backgroundColor: '#1790ed',
-                    }}>
+                <div className={"chitchat-header-row"}>
+                    <div className={"chitchat-header-pic"}>
                         <Avatar size={40} style={{backgroundColor: '#ffffff'}} icon={<UserOutlined/>}
                                 src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
 
-                        <Input style={{
-                            backgroundColor: '#1790ed',
-                            width: "50%",
-                            height: 24,
-                            borderRadius: 12,
-                            display: "flex",
-                            alignItems: "center",
-                            paddingLeft: 10
-                        }} prefix={<SearchOutlined style={{color: "#d3d3d3"}}/>}/>
+                        <Input className={"chitchat-header-search-input"}
+                               prefix={<SearchOutlined style={{color: "#d3d3d3"}}/>}/>
                     </div>
-                    <div style={{
-                        width: "50%",
-                        backgroundColor: '#1790ed',
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "center",
-                        color: '#ffffff',
-                        fontSize: 26
-                    }}>
+                    <div className={"chitchat-header-check-menu"}>
                         <CommentOutlined style={{width: 60}}/>
-                        <TeamOutlined style={{width: 60}}/>
-                        <CloudOutlined style={{width: 60}}/>
+                        <TeamOutlined style={{width: 60, color: "#bfbfbf"}}/>
+                        <CloudOutlined style={{width: 60, color: "#bfbfbf"}}/>
                     </div>
 
-                    <div style={{
-                        width: "25%",
-                        backgroundColor: '#3448a1',
-                        borderTopRightRadius: 5,
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-evenly",
-                        color: '#ffffff'
-                    }}>
+                    <div className={"chitchat-header-check-tip"}>
                         <MenuOutlined style={{marginLeft: '40%'}}/>
                         <MinusOutlined/>
                         <BorderOutlined/>
@@ -150,62 +110,27 @@ class ChatFrame extends Component<Props> {
                 </div>
 
                 {/*待办列*/}
-                <div style={{
-                    textAlign: "center",
-                    width: 825,
-                    height: 40,
-                    margin: '0 auto',
-                    display: "flex",
-                    flexDirection: "row",
-                    borderLeft: "1px solid #eaeaea"
-                }}>
-                    <NavLink
-                        style={{
-                            width: "25%",
-                            display: "flex",
-                            alignItems: "center",
-                            justifyContent: "space-between",
-                            padding: '0px 10px',
-                            marginTop: 2,
-                            backgroundColor: '#fafafa',
-                            borderBottom: '1px solid #eaeaea',
-                        }}
-                        to={"/"}
-                        onMouseOver={() => {
-                            this.isSelectedToDoThings = true;
-                        }}
-                        onMouseOut={() => {
-                            this.isSelectedToDoThings = false
-                        }}
-                        activeStyle={{backgroundColor: this.isSelectedToDoThings ? '#ebebec' : '#fafafa'}}
+                <div className={"chitchat-todo-list-row"}>
+                    <NavLink className={"chitchat-todo-list"}
+                             to={"/"}
+                             onMouseOver={() => {
+                                 this.isSelectedToDoThings = true;
+                             }}
+                             onMouseOut={() => {
+                                 this.isSelectedToDoThings = false
+                             }}
+                             activeStyle={{backgroundColor: this.isSelectedToDoThings ? '#ebebec' : '#fafafa'}}
                     >
                         <div style={{display: "flex", alignItems: "center"}}>
                             <CheckCircleOutlined style={{fontSize: 18, color: '#232323'}}/>
                             <span style={{color: '#000000', marginLeft: 8}}>待办 . 16</span>
                         </div>
                     </NavLink>
-                    <div style={{
-                        width: "55%",
-                        display: "flex",
-                        alignItems: "center",
-                        marginLeft: 2,
-                        paddingLeft: 10,
-                        fontSize: 16,
-                        color: '#000000',
-                        borderBottom: '1px solid #eaeaea'
-                    }}>
+                    <div className={"chitchat-object"}>
                         小杨同学
                     </div>
 
-                    <div style={{
-                        width: "20%",
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-around",
-                        borderBottom: '1px solid #eaeaea',
-                        fontSize: 16,
-                        borderRight: "1px solid #eaeaea"
-                    }}>
+                    <div className={"chitchat-check-operation"}>
                         <VideoCameraOutlined/>
                         <PhoneOutlined/>
                         <PlusCircleOutlined/>
@@ -214,64 +139,23 @@ class ChatFrame extends Component<Props> {
                 </div>
 
                 {/*联系人与聊天列*/}
-                <div style={{
-                    textAlign: "center",
-                    width: 825,
-                    height: 550,
-                    margin: '0 auto',
-                    display: "flex",
-                    flexDirection: "row",
-                    borderLeft: "1px solid #eaeaea",
-                    borderBottom: "1px solid #eaeaea"
-                }}>
+                <div className={"linkman-and-chat-content-row"}>
                     {/*联系人列表*/}
-                    <div style={{
-                        width: '25%',
-                        backgroundColor: '#fafafa',
-                        borderBottomLeftRadius: 5,
-                        fontSize: 13
-                    }}>
-                        <div style={{
-                            display: "flex",
-                            flexDirection: "row",
-                            padding: "10px 10px 10px 15px",
-                            backgroundColor: "white"
-                        }}>
+                    <div className={"linkman-list"}>
+                        <div className={"linkman-item"}>
 
                             <Avatar size={40} style={{backgroundColor: "gray"}} icon={<UserOutlined/>}
                                     src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
-                            <div style={{
-                                display: "flex",
-                                flexDirection: "column",
-                                marginLeft: 10,
-                                textAlign: "left",
-                                minWidth: 130
-                            }}>
-                                <div style={{
-                                    display: "flex",
-                                    flexDirection: "row",
-                                    alignItems: "center",
-                                    justifyContent: "space-between"
-                                }}>
-                                    <div style={{
-                                        maxWidth: 77,
-                                        whiteSpace: "nowrap",
-                                        overflow: "hidden",
-                                        textOverflow: "ellipsis",
-                                        color: "black",
-                                        fontWeight: 500
-                                    }}>
+                            <div className={"linkman-item-content"}>
+                                <div className={"linkman-info"}>
+                                    <div className={"linkman-name"}>
                                         小杨同学
                                     </div>
-                                    <div style={{float: "right", fontSize: 12, maxWidth: 53}}>下午 5:24</div>
+                                    <div className={"linkman-send-time"}>下午 5:24</div>
                                 </div>
-                                <div style={{display: "flex", flexDirection: "row", fontSize: 12}}>
-                                    <div style={{
-                                        maxWidth: 130,
-                                        whiteSpace: "nowrap",
-                                        overflow: "hidden",
-                                        textOverflow: "ellipsis"
-                                    }}>你好啊，我的名字叫react，很高兴认识你！
+                                <div className={"linkman-chat-content"}>
+                                    <div className={"linkman-chat-content-info"}>
+                                        你好啊，我的名字叫react，很高兴认识你！
                                     </div>
                                 </div>
                             </div>
@@ -280,48 +164,34 @@ class ChatFrame extends Component<Props> {
                     </div>
 
                     {/*聊天框*/}
-                    <div style={{width: '75%', borderRight: "1px solid #eaeaea"}}>
-                        <div style={{height: '75%', display: "flex", flexDirection: "column", overflow: "auto"}}>
-                            <Link to={""} style={{fontSize: 13, marginTop: 15}}>
+                    <div className={"chitchat-frame"}>
+                        <div className={"chitchat-frame-content"}>
+                            <Link to={""} className={"check-history-info"}>
                                 <ClockCircleFilled/>
                                 &nbsp;查看历史消息
                             </Link>
 
-                            <div style={{fontSize: 12, color: "#bababc", marginTop: 20}}>
+                            <div className={"chitchat-time"}>
                                 &nbsp;{moment().format('a HH:mm:ss')}
                             </div>
 
                             {/*聊天对象*/}
-                            <div style={{
-                                width: '94%',
-                                marginLeft: "3%",
-                                marginTop: 5,
-                                display: "flex",
-                                flexDirection: "row",
-                                justifyContent: "flex-start",
-                                alignItems: "flex-start"
-                            }}>
+                            <div className={"chitchat-send-message-row"}
+                                 style={{alignItems: "center", justifyContent: "flex-start"}}>
                                 <div>
                                     <Avatar size={40} style={{backgroundColor: "gray"}} icon={<UserOutlined/>}
                                             src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
                                 </div>
                                 <div className={"chitchat-other"}>
-                                    <span>你好啊，我的名字叫react，很高兴认识你！</span>
+                                    <span style={{borderRadius: 7}}>你好啊，我的名字叫react，很高兴认识你！</span>
                                 </div>
                             </div>
 
                             {/*我*/}
-                            <div style={{
-                                width: '94%',
-                                marginLeft: "3%",
-                                marginTop: 5,
-                                display: "flex",
-                                flexDirection: "row",
-                                justifyContent: "flex-end",
-                                alignItems: "flex-end"
-                            }}>
+                            <div className={"chitchat-send-message-row"}
+                                 style={{justifyContent: "flex-end", alignItems: "center"}}>
                                 <div className={"chitchat-me"}>
-                                    <span>有多高兴？</span>
+                                    <span style={{borderRadius: 7}}>有多高兴？</span>
                                 </div>
                                 <div>
                                     <Avatar size={40} style={{backgroundColor: "white"}} icon={<UserOutlined/>}
@@ -331,22 +201,10 @@ class ChatFrame extends Component<Props> {
 
                         </div>
 
-                        <div style={{
-                            height: '25%',
-                            borderTop: '1px solid #eaeaea',
-                            display: "flex",
-                            flexDirection: "column",
-                            marginLeft: 2
-                        }}>
+                        <div className={"send-message-row"}>
                             {/*发送消息设置列*/}
-                            <div style={{height: '25%', display: "flex", flexDirection: "row", alignItems: "center"}}>
-                                <div style={{
-                                    width: '55%',
-                                    display: "flex",
-                                    flexDirection: "row",
-                                    justifyContent: "space-around",
-                                    fontSize: 18
-                                }}>
+                            <div className={"send-message-setting-row"}>
+                                <div className={"send-message-icons"}>
                                     <FontColorsOutlined/>
                                     <SmileOutlined/>
                                     <ScissorOutlined/>
@@ -356,7 +214,7 @@ class ChatFrame extends Component<Props> {
                                     <CalendarOutlined/>
                                     <HeartOutlined/>
                                 </div>
-                                <div style={{width: '42%', display: "flex", justifyContent: "flex-end", fontSize: 13}}>
+                                <div className={"message-history"}>
                                     <span style={{cursor: "pointer"}} className="ant-dropdown-link">
                                         消息记录&nbsp;
                                         <DownOutlined/>
@@ -366,23 +224,10 @@ class ChatFrame extends Component<Props> {
 
                             {/*输入消息编辑框*/}
                             <div style={{height: '55%'}}>
-                                <TextArea style={{
-                                    backgroundColor: '#ffffff',
-                                    border: "none",
-                                    boxShadow: "none",
-                                    height: '100%',
-                                    fontSize: 13,
-                                    color: "black"
-                                }}>
+                                <TextArea className={"edit-message-textarea"}>
                                 </TextArea>
                             </div>
-                            <div style={{
-                                height: '25%',
-                                width: '97%',
-                                display: "flex",
-                                justifyContent: "flex-end",
-                                alignItems: "center",
-                            }}>
+                            <div className={"message-send-row"}>
                                 <Button size={"small"} type={"primary"} style={{fontSize: 13}}
                                         className="ant-dropdown-link">
                                     发送(
